59a06b9080dc589b7968b62c326e7df8ab85bada
DatNguyen <datnguyen9240@gmail.com>
2024-10-27 19:08:41 +0700

    update
    update

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import { useEffect } from 'react';
 
-const baseUrl = "http://sharetaxi.somee.com/api/";
+const baseUrl = "https://admin-share-taxi.vercel.app/";
 const api = axios.create({
   baseURL: baseUrl,
 });
